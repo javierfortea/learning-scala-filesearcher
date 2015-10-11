c230079835b10e3fada7453116090b469f0bc0f8
javierfortea <fcojavierfortea@gmail.com>
2015-10-11 19:12:24 +0100

    Adding object to write result to a file.

---
+++ src/main/scala/fileSearcher/SearchResultWriter.scala
@@ -0,0 +1,27 @@
+package fileSearcher
+
+import java.io.FileWriter
+import java.io.PrintWriter
+
+object SearchResultWriter {
+  
+  def writeToFile(filePath: String, searchResults: List[(String, Option[Int])]) = {
+    
+    val fileWriter = new FileWriter(filePath)
+    val printWriter = new PrintWriter(fileWriter)
+    
+    try
+      for ((fileName, countOption) <- searchResults)
+        printWriter.println(
+            countOption match {
+              case Some(count) => s"$fileName -> $count"
+              case None => s"$fileName"
+            }
+        )
+    finally {
+      printWriter.close()
+      fileWriter.close()
+    }
+  }
+
+}
